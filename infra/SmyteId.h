1a4f552d344717f46ea2d0436d0ca6b17f7f5c03
Yunjing Xu <yunjing@smyte.com>
2017-02-18 00:27:59 +0000

    Generate smyte id from kafka offset and timestamp
    Test Plan: Test cases included
    
    Reviewers: paul, derrickrburns, josh
    
    Reviewed By: josh
    
    Differential Revision: https://phabricator-svc.smyte.com/D3085

--- infra/SmyteId.h
+++ infra/SmyteId.h
@@ -4,14 +4,58 @@
 #include <string>
 
 #include "boost/endian/buffers.hpp"
+#include "glog/logging.h"
 
 namespace infra {
 
 class SmyteId {
  public:
+  // Total 64 bits for smyte id: Sign:1 TimeMs:40 Unique:10 Machine:13
+  // 40 bits local machine increment
+  static constexpr int kTimestampBits = 40;
+  static constexpr int64_t kTimestampSize = 1L << kTimestampBits;
+  static constexpr int64_t kTimestampEpoch = 1262304000000;  // 2010-01-01
+
+  // 10 bits local machine increment
+  static constexpr int kUniqueBits = 10;
+  static constexpr int64_t kUniqueSize = 1L << kUniqueBits;
+
+  // 13 bits machine id
+  static constexpr int kMachineBits = 13;
+  static constexpr int64_t kMachineSize = 1L << kMachineBits;
+  static constexpr int64_t kMachineBase = kMachineSize - 1024;  // use the last 1024 number
+
+  // Configurations for virtual sharding
+  static constexpr int kVirtualShardBits = 10;
+  static constexpr int kVirtualShardCount = 1L << kVirtualShardBits;
+
+  // Generate a smyte id deterministically from kafka offset, timestamp and virtual shard.
+  static SmyteId generateFromKafka(int64_t kafkaOffset, int64_t timestampMs, int virtualShard) {
+    int64_t shiftedTimestamp = timestampMs - SmyteId::kTimestampEpoch;
+    CHECK(shiftedTimestamp >= 0 && shiftedTimestamp < kTimestampSize)
+        << "timestamp " << timestampMs << " for kafka offset " << kafkaOffset << " is out of range";
+    CHECK(virtualShard >= 0 && virtualShard < kVirtualShardCount)
+        << "virtual shard " << virtualShard << " for kafka offset " << kafkaOffset << " is out of range";
+    int64_t unique = kafkaOffset % SmyteId::kUniqueSize;
+    int64_t machine = SmyteId::kMachineBase + virtualShard;
+    int64_t smyteId = (((shiftedTimestamp << SmyteId::kUniqueBits) + unique) << SmyteId::kMachineBits) + machine;
+
+    return SmyteId(smyteId);
+  }
+
   explicit SmyteId(int64_t smyteId) : smyteId_(smyteId) {}
 
-  // Append the smyte id to the output as an eight byte string. Use big endian so that it can be sorted as string.
+  explicit SmyteId(const std::string& smyteId)
+      : smyteId_(boost::endian::detail::load_big_endian<int64_t, sizeof(int64_t)>(smyteId.data())) {}
+
+  // Encode smyte id as an an 8-byte string. Use big endian so that it can be sorted in numerical order.
+  std::string asBinary() const {
+    std::string result;  // any reasonable compiler would apply Return Value Optimization here
+    appendAsBinary(&result);
+    return result;
+  }
+
+  // Append the smyte id to the output as an 8-byte string. Use big endian so that it can be sorted in numerical order.
   void appendAsBinary(std::string* out) const {
     boost::endian::big_int64_buf_t value(smyteId_);
     out->append(value.data(), sizeof(int64_t));
@@ -21,9 +65,18 @@ class SmyteId {
     return (smyteId_ ^ (smyteId_ >> kMachineBits)) % shardCount;
   }
 
- private:
-  static constexpr int kMachineBits = 13;
+  // Return the virtual shard number [0, 1024) and -1 for error.
+  int getVirtualShard() const {
+    int64_t machine = smyteId_ % kMachineSize;
+    if (machine < kMachineBase) return -1;
+    return machine % kVirtualShardCount;
+  }
 
+  bool operator==(const SmyteId& rhs) const {
+    return smyteId_ == rhs.smyteId_;
+  }
+
+ private:
   int64_t smyteId_;
 };
 
--- infra/SmyteIdTest.cpp
+++ infra/SmyteIdTest.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <limits>
 
 #include "gtest/gtest.h"
@@ -14,6 +15,46 @@ TEST(SmyteIdTest, GetShardIndex) {
 
   EXPECT_EQ(4, SmyteId(std::numeric_limits<int64_t>::max()).getShardIndex(20));
   EXPECT_EQ(4, SmyteId(std::numeric_limits<int64_t>::max()).getShardIndex(10));
+}
+
+TEST(SmyteIdTest, AsBinary) {
+  std::string smyteIdBinary = SmyteId(12345).asBinary();
+  EXPECT_EQ(SmyteId(12345), SmyteId(smyteIdBinary));
+}
+
+TEST(SmyteIdTest, AppendAsBinary) {
+  std::string smyteId;
+  SmyteId(12345).appendAsBinary(&smyteId);
+  EXPECT_EQ(SmyteId(12345), SmyteId(smyteId));
+}
+
+TEST(SmyteIdTest, GenerateFromKafkaBadInput) {
+  EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampEpoch - 10, 10),
+               "Check failed.*timestamp 1262303999990 for kafka offset 123 is out of range");
+  EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampSize, 10),
+               "Check failed.*timestamp 1099511627776 for kafka offset 123 is out of range");
+  EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampSize + 10, 10),
+               "Check failed.*timestamp 1099511627786 for kafka offset 123 is out of range");
+
+  EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampEpoch + 100, -1),
+               "Check failed.*virtual shard -1 for kafka offset 123 is out of range");
+  EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampEpoch + 100, SmyteId::kVirtualShardCount),
+               "Check failed.*virtual shard 1024 for kafka offset 123 is out of range");
+    EXPECT_DEATH(SmyteId::generateFromKafka(123, SmyteId::kTimestampEpoch + 100, SmyteId::kVirtualShardCount + 10),
+               "Check failed.*virtual shard 1034 for kafka offset 123 is out of range");
+}
+
+TEST(SmyteId, GenerateFromKafkaGoodInput) {
+  EXPECT_EQ(SmyteId::generateFromKafka(0, SmyteId::kTimestampEpoch, 0), SmyteId(7168));
+  EXPECT_EQ(SmyteId::generateFromKafka(123, SmyteId::kTimestampEpoch, 10), SmyteId(1014794));
+  EXPECT_EQ(SmyteId::generateFromKafka(12345, SmyteId::kTimestampEpoch + 10, 1023), SmyteId(84361215));
+  int64_t nowMs =
+      std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
+          .count();
+  EXPECT_EQ(SmyteId::generateFromKafka(123456, nowMs, 1000),
+            SmyteId((nowMs - SmyteId::kTimestampEpoch) << (SmyteId::kUniqueBits + SmyteId::kMachineBits) |
+                    (123456 % SmyteId::kUniqueSize) << SmyteId::kMachineBits |
+                    (SmyteId::kMachineSize - SmyteId::kVirtualShardCount + 1000)));
 }
 
 }  // namespace infra
