f94500895684d7de5fb423912e5ddef9c0cab511
Yunjing Xu <yunjing@smyte.com>
2017-02-27 23:05:07 +0000

    Support column family group in db tests

--- stesting/TestWithRocksDb.h
+++ stesting/TestWithRocksDb.h
@@ -9,6 +9,7 @@
 #include <vector>
 
 #include "boost/filesystem.hpp"
+#include "folly/Format.h"
 #include "glog/logging.h"
 #include "gtest/gtest.h"
 #include "pipeline/DatabaseManager.h"
@@ -25,6 +26,8 @@ class TestWithRocksDb : public ::testing::Test {
   using RocksDbConfigurator = void (*)(int, rocksdb::ColumnFamilyOptions*);
   // Map column family names to RocksDbConfigurators
   using RocksDbConfiguratorMap = std::unordered_map<std::string, RocksDbConfigurator>;
+  // Map column family group name to group count
+  using RocksDbCfGroupConfigMap = std::unordered_map<std::string, size_t>;
 
   static void SetUpTestCase() {
     static bool initialized = false;
@@ -45,9 +48,11 @@ class TestWithRocksDb : public ::testing::Test {
   // Column families named in optionalColumnFamilyNames will either be created with a provided configurator or use a
   // default configuration. Either, they are guaranteed to exist.
   explicit TestWithRocksDb(std::vector<std::string> optionalColumnFamilyNames = {},
-                           RocksDbConfiguratorMap rocksDbConfiguratorMap = RocksDbConfiguratorMap())
+                           RocksDbConfiguratorMap rocksDbConfiguratorMap = RocksDbConfiguratorMap(),
+                           RocksDbCfGroupConfigMap rocksDbCfGroupConfigMap = RocksDbCfGroupConfigMap())
       : columnFamilyNames_(std::move(optionalColumnFamilyNames)),
-        rocksDbConfiguratorMap_(std::move(rocksDbConfiguratorMap)) {
+        rocksDbConfiguratorMap_(std::move(rocksDbConfiguratorMap)),
+        rocksDbCfGroupConfigMap_(std::move(rocksDbCfGroupConfigMap)) {
     // add the required column family names
     columnFamilyNames_.emplace_back("default");
     columnFamilyNames_.emplace_back("smyte-metadata");
@@ -84,13 +89,32 @@ class TestWithRocksDb : public ::testing::Test {
         // 1MB default cache, it makes no difference for testing really
         rocksDbConfiguratorMap_[name](1, &columnFamilyOptions);
       }
-      rocksdb::ColumnFamilyHandle* columnFamily;
-      rocksdb::Status s = db_->CreateColumnFamily(columnFamilyOptions, name, &columnFamily);
-      CHECK(s.ok()) << "Creating column family `" << name << "` failed: " << s.ToString();
-      columnFamilyMap_[name] = columnFamily;
+      if (rocksDbCfGroupConfigMap_.count(name) > 0) {
+        // create column family group
+        std::vector<rocksdb::ColumnFamilyHandle*> group;
+        for (size_t i = 0; i < rocksDbCfGroupConfigMap_[name]; i++) {
+          auto cfName = folly::sformat("{}-{}", name, i);
+          rocksdb::ColumnFamilyHandle* columnFamily;
+          rocksdb::Status s = db_->CreateColumnFamily(columnFamilyOptions, cfName, &columnFamily);
+          CHECK(s.ok()) << "Creating column family `" << cfName << "` failed: " << s.ToString();
+          columnFamilyMap_[cfName] = columnFamily;
+          group.push_back(columnFamily);
+        }
+        columnFamilyGroupMap_[name] = std::move(group);
+      } else {
+        // create a standalone column family
+        rocksdb::ColumnFamilyHandle* columnFamily;
+        rocksdb::Status s = db_->CreateColumnFamily(columnFamilyOptions, name, &columnFamily);
+        CHECK(s.ok()) << "Creating column family `" << name << "` failed: " << s.ToString();
+        columnFamilyMap_[name] = columnFamily;
+      }
     }
 
-    databaseManager_ = newDatabaseManager(columnFamilyMap_, db_);
+    if (columnFamilyGroupMap_.empty()) {
+      databaseManager_ = newDatabaseManager(columnFamilyMap_, db_);
+    } else {
+      databaseManager_ = newDatabaseManager(columnFamilyMap_, columnFamilyGroupMap_, db_);
+    }
   }
 
   void TearDown() override {
@@ -108,6 +132,12 @@ class TestWithRocksDb : public ::testing::Test {
     return std::make_shared<pipeline::DatabaseManager>(columnFamilyMap, true, db);
   }
 
+  virtual std::shared_ptr<pipeline::DatabaseManager> newDatabaseManager(
+      const pipeline::DatabaseManager::ColumnFamilyMap& columnFamilyMap,
+      const pipeline::DatabaseManager::ColumnFamilyGroupMap& columnFamilyGroupMap, rocksdb::DB* db) {
+    return std::make_shared<pipeline::DatabaseManager>(columnFamilyMap, columnFamilyGroupMap, true, db);
+  }
+
   rocksdb::DB* db() const {
     return db_;
   }
@@ -123,6 +153,12 @@ class TestWithRocksDb : public ::testing::Test {
   rocksdb::ColumnFamilyHandle* columnFamily(const std::string& name) {
     CHECK_GT(columnFamilyMap_.count(name), 0);
     return columnFamilyMap_[name];
+  }
+
+  rocksdb::ColumnFamilyHandle* columnFamily(const std::string& groupName, int index) {
+    CHECK_GT(columnFamilyGroupMap_.count(groupName), 0);
+    CHECK_GT(columnFamilyGroupMap_[groupName].size(), index);
+    return columnFamilyGroupMap_[groupName][index];
   }
 
   size_t totalKeyCount(rocksdb::ColumnFamilyHandle* columnFamilyHandle = nullptr) const {
@@ -145,10 +181,12 @@ class TestWithRocksDb : public ::testing::Test {
  private:
   std::vector<std::string> columnFamilyNames_;
   RocksDbConfiguratorMap rocksDbConfiguratorMap_;
+  RocksDbCfGroupConfigMap rocksDbCfGroupConfigMap_;
   rocksdb::DB* db_ = nullptr;
   std::shared_ptr<pipeline::DatabaseManager> databaseManager_;
   boost::filesystem::path dbPath_;
   pipeline::DatabaseManager::ColumnFamilyMap columnFamilyMap_;
+  pipeline::DatabaseManager::ColumnFamilyGroupMap columnFamilyGroupMap_;
 };
 
 }  // namespace stesting
