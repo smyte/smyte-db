b7ceb6f48a7394441cc014c22e33ac741f05e0a7
Derrick R. Burns <derrickrburns@smyte.com>
2017-03-17 20:43:52 +0000

    Pass back arrays of item/score arrays.

--- pipeline/BuildVersion.cpp
+++ pipeline/BuildVersion.cpp
@@ -3,8 +3,8 @@
 namespace pipeline {
 
 // We expect external tools invoking bazel build would update these values.
-const char* kSmyteBuildGitSha = "SMYTE_BUILD_GIT_SHA";
-const char* kSmyteBuildGitTime = "SMYTE_BUILD_GIT_TIME";
-const char* kSmyteBuildCompileTime = "SMYTE_BUILD_COMPILE_TIME";
+const char* kSmyteBuildGitSha = "d5f56b2c66f3efa3a4fc33b14c2b46be7af74f09";
+const char* kSmyteBuildGitTime = "2017-03-17T20:24:09+00:00";
+const char* kSmyteBuildCompileTime = "2017-03-17T20:38:24+00:00";
 
 }  // namespace pipeline
